b50fd3a4f78b2f13f5c57d79a068e8235373217c
Nathan Griffiths <ngriffiths@postmedia.com>
2022-12-26 17:30:39 -0800

    Updates

---
+++ main.9bfda0fb.chunk.js
@@ -0,0 +1,289 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ 123:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 124:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 125:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 126:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 127:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 128:
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+
+/***/ 79:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var core_js_modules_es_array_concat__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(80);
+/* harmony import */ var core_js_modules_es_array_concat__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_array_concat__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var core_js_modules_es_array_filter__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(91);
+/* harmony import */ var core_js_modules_es_array_filter__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_array_filter__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var core_js_modules_es_array_for_each__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(92);
+/* harmony import */ var core_js_modules_es_array_for_each__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_array_for_each__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var core_js_modules_es_array_includes__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(94);
+/* harmony import */ var core_js_modules_es_array_includes__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_array_includes__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var core_js_modules_es_array_iterator__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(33);
+/* harmony import */ var core_js_modules_es_array_iterator__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_array_iterator__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var core_js_modules_es_object_to_string__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(99);
+/* harmony import */ var core_js_modules_es_object_to_string__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_object_to_string__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var core_js_modules_es_regexp_exec__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(68);
+/* harmony import */ var core_js_modules_es_regexp_exec__WEBPACK_IMPORTED_MODULE_6___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_regexp_exec__WEBPACK_IMPORTED_MODULE_6__);
+/* harmony import */ var core_js_modules_es_regexp_to_string__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(102);
+/* harmony import */ var core_js_modules_es_regexp_to_string__WEBPACK_IMPORTED_MODULE_7___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_regexp_to_string__WEBPACK_IMPORTED_MODULE_7__);
+/* harmony import */ var core_js_modules_es_string_includes__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(103);
+/* harmony import */ var core_js_modules_es_string_includes__WEBPACK_IMPORTED_MODULE_8___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_string_includes__WEBPACK_IMPORTED_MODULE_8__);
+/* harmony import */ var core_js_modules_es_string_iterator__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(71);
+/* harmony import */ var core_js_modules_es_string_iterator__WEBPACK_IMPORTED_MODULE_9___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_string_iterator__WEBPACK_IMPORTED_MODULE_9__);
+/* harmony import */ var core_js_modules_es_string_replace__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(106);
+/* harmony import */ var core_js_modules_es_string_replace__WEBPACK_IMPORTED_MODULE_10___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_string_replace__WEBPACK_IMPORTED_MODULE_10__);
+/* harmony import */ var core_js_modules_es_string_search__WEBPACK_IMPORTED_MODULE_11__ = __webpack_require__(107);
+/* harmony import */ var core_js_modules_es_string_search__WEBPACK_IMPORTED_MODULE_11___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_string_search__WEBPACK_IMPORTED_MODULE_11__);
+/* harmony import */ var core_js_modules_es_string_split__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(109);
+/* harmony import */ var core_js_modules_es_string_split__WEBPACK_IMPORTED_MODULE_12___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_es_string_split__WEBPACK_IMPORTED_MODULE_12__);
+/* harmony import */ var core_js_modules_web_dom_collections_for_each__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(111);
+/* harmony import */ var core_js_modules_web_dom_collections_for_each__WEBPACK_IMPORTED_MODULE_13___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_web_dom_collections_for_each__WEBPACK_IMPORTED_MODULE_13__);
+/* harmony import */ var core_js_modules_web_dom_collections_iterator__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(112);
+/* harmony import */ var core_js_modules_web_dom_collections_iterator__WEBPACK_IMPORTED_MODULE_14___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_web_dom_collections_iterator__WEBPACK_IMPORTED_MODULE_14__);
+/* harmony import */ var core_js_modules_web_url__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(113);
+/* harmony import */ var core_js_modules_web_url__WEBPACK_IMPORTED_MODULE_15___default = /*#__PURE__*/__webpack_require__.n(core_js_modules_web_url__WEBPACK_IMPORTED_MODULE_15__);
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(40);
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_16___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_16__);
+/* harmony import */ var regenerator_runtime_runtime__WEBPACK_IMPORTED_MODULE_17__ = __webpack_require__(76);
+/* harmony import */ var regenerator_runtime_runtime__WEBPACK_IMPORTED_MODULE_17___default = /*#__PURE__*/__webpack_require__.n(regenerator_runtime_runtime__WEBPACK_IMPORTED_MODULE_17__);
+/* harmony import */ var _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_18__ = __webpack_require__(77);
+/* harmony import */ var _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_18___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_18__);
+/* harmony import */ var lazysizes__WEBPACK_IMPORTED_MODULE_19__ = __webpack_require__(122);
+/* harmony import */ var lazysizes__WEBPACK_IMPORTED_MODULE_19___default = /*#__PURE__*/__webpack_require__.n(lazysizes__WEBPACK_IMPORTED_MODULE_19__);
+/* harmony import */ var papaparse__WEBPACK_IMPORTED_MODULE_20__ = __webpack_require__(78);
+/* harmony import */ var papaparse__WEBPACK_IMPORTED_MODULE_20___default = /*#__PURE__*/__webpack_require__.n(papaparse__WEBPACK_IMPORTED_MODULE_20__);
+/* harmony import */ var _css_normalize_css__WEBPACK_IMPORTED_MODULE_21__ = __webpack_require__(123);
+/* harmony import */ var _css_normalize_css__WEBPACK_IMPORTED_MODULE_21___default = /*#__PURE__*/__webpack_require__.n(_css_normalize_css__WEBPACK_IMPORTED_MODULE_21__);
+/* harmony import */ var _css_postmedia_css__WEBPACK_IMPORTED_MODULE_22__ = __webpack_require__(124);
+/* harmony import */ var _css_postmedia_css__WEBPACK_IMPORTED_MODULE_22___default = /*#__PURE__*/__webpack_require__.n(_css_postmedia_css__WEBPACK_IMPORTED_MODULE_22__);
+/* harmony import */ var _css_colors_css__WEBPACK_IMPORTED_MODULE_23__ = __webpack_require__(125);
+/* harmony import */ var _css_colors_css__WEBPACK_IMPORTED_MODULE_23___default = /*#__PURE__*/__webpack_require__.n(_css_colors_css__WEBPACK_IMPORTED_MODULE_23__);
+/* harmony import */ var _css_fonts_css__WEBPACK_IMPORTED_MODULE_24__ = __webpack_require__(126);
+/* harmony import */ var _css_fonts_css__WEBPACK_IMPORTED_MODULE_24___default = /*#__PURE__*/__webpack_require__.n(_css_fonts_css__WEBPACK_IMPORTED_MODULE_24__);
+/* harmony import */ var _css_main_css__WEBPACK_IMPORTED_MODULE_25__ = __webpack_require__(127);
+/* harmony import */ var _css_main_css__WEBPACK_IMPORTED_MODULE_25___default = /*#__PURE__*/__webpack_require__.n(_css_main_css__WEBPACK_IMPORTED_MODULE_25__);
+/* harmony import */ var _css_nav_css__WEBPACK_IMPORTED_MODULE_26__ = __webpack_require__(128);
+/* harmony import */ var _css_nav_css__WEBPACK_IMPORTED_MODULE_26___default = /*#__PURE__*/__webpack_require__.n(_css_nav_css__WEBPACK_IMPORTED_MODULE_26__);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// LIBS
+
+ // CSS
+
+
+
+
+
+
+ // VARS
+
+var headerOffset = 50;
+var sections = ['main', 'map', 'drugs', 'demo', 'footer'];
+var bignumURL = 'https://raw.githubusercontent.com/vs-postmedia/bccdc-od-deaths-scraper/main/data/topline-numbers.csv'; // JS
+
+var init = /*#__PURE__*/function () {
+  var _ref = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_18___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_16___default.a.mark(function _callee() {
+    var selectedSection;
+    return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_16___default.a.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            // get URL params
+            selectedSection = getUrlParams();
+
+            if (sections.includes(selectedSection)) {
+              sections.forEach(function (d) {
+                if (d !== selectedSection) {
+                  var elem = document.getElementById(d);
+                  elem.parentNode.removeChild(elem);
+                }
+
+                if (d == 'main') {
+                  getToplineNumbers();
+                }
+              });
+            } else {
+              // nav menu
+              setupMenu(); // topline bignums
+
+              getToplineNumbers();
+            }
+
+          case 2:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function init() {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+function getToplineNumbers() {
+  // fetch data & update big numbers section
+  papaparse__WEBPACK_IMPORTED_MODULE_20__["parse"](bignumURL, {
+    download: true,
+    header: false,
+    complete: function complete(results) {
+      setupBigNums(results.data);
+    }
+  });
+}
+
+function getUrlParams() {
+  // accepted params are: main, map, drugs, demo
+  var urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get('section');
+}
+
+function setupBigNums(data) {
+  var main = document.getElementById('main-big-num');
+  var drug = document.getElementById('drugs-big-num'); // timestamp
+
+  var last_update = data.filter(function (array) {
+    return array.includes('last_update');
+  });
+  var ts = setupTimestamp(last_update[0][1]); // build main big num section
+
+  setupBigNum(data, main, ts, last_update); // build drugs big num section
+  // HAVE TO FIGURE OUT WHERE TO GET THESE FIGS FROM
+  // setupDrugsBigNum(data, drug, ts);
+}
+
+function setupBigNum(data, main, timestamp, last_update) {
+  // setup wrapper div
+  var mainDiv = document.createElement('div');
+  mainDiv.className = 'stat-box'; // data
+
+  var new_deaths = data.filter(function (array) {
+    return array.includes('deaths_new');
+  });
+  var total_deaths = data.filter(function (array) {
+    return array.includes('deaths_total');
+  });
+  var daily_deaths = data.filter(function (array) {
+    return array.includes('deaths_daily');
+  }); // template
+
+  mainDiv.innerHTML = "\n\t\t<div class=\"stat\">\n\t\t\t<p class=\"big-num\">".concat(new_deaths[0][1], "</p>\n\t\t\t<p class=\"label\">New deaths in ").concat(last_update[0][1].split(' ')[0], "</p>\n\t\t</div>\n\t\t<div class=\"stat\">\n\t\t\t<p class=\"big-num\">").concat(numberWithCommas(total_deaths[0][1]), "</p>\n\t\t\t<p class=\"label\">Deaths since 2016</p>\n\t\t</div>\n\t\t<div class=\"stat\">\n\t\t\t<p class=\"big-num\">").concat(daily_deaths[0][1], "</p>\n\t\t\t<p class=\"label\">Deaths per day (average)</p>\n\t\t</div>\n\t"); // update the DOM
+
+  main.appendChild(mainDiv);
+  main.appendChild(timestamp);
+}
+
+function setupDrugsBigNum(data, drug, timestamp) {
+  // setup wrapper div
+  var drugDiv = document.createElement('div');
+  drugDiv.className = 'stat-box';
+  var fenty_deaths = data.filter(function (array) {
+    return array.includes('deaths_fentanyl');
+  });
+  var benzo_deaths = data.filter(function (array) {
+    return array.includes('deaths_benzo');
+  });
+  drugDiv.innerHTML = "\n\t\t<div class=\"stat\">\n\t\t\t<p class=\"big-num\">".concat(benzo_deaths[0][1], "</p>\n\t\t\t<p class=\"label\">Deaths involving benzodiazepines</p>\n\t\t</div>\n\t\t<div class=\"stat\">\n\t\t\t<p class=\"big-num\">").concat(fenty_deaths[0][1], "</p>\n\t\t\t<p class=\"label\">Deaths involving extreme fentanyl concentrations</p>\n\t\t</div>\n\t");
+  drug.appendChild(drugDiv);
+  drug.appendChild(timestamp);
+}
+
+function setupMenu() {
+  var navbar = document.createElement('nav'); // template
+
+  navbar.innerHTML = "\n\t\t<div id=\"nav-container\">\n\t\t<ul>\n\t\t\t<li class=\"nav-li active-nav\">\n\t\t\t\t<a href=\"#main\">Main</a>\n\t\t\t</li>\n\t\t\t<li class=\"nav-li\">\n\t\t\t\t<a href=\"#map\">Map</a>\n\t\t\t</li>\n\t\t\t<li class=\"nav-li\">\n\t\t\t\t<a href=\"#drugs\">Drugs</a>\n\t\t\t</li>\n\t\t\t<li class=\"nav-li\">\n\t\t\t\t<a href=\"#demo\">Who\u2019s dying</a>\n\t\t\t</li>\n\t\t</ul>\n\t</div>\n\t"; // update the DOM
+
+  main.appendChild(navbar);
+  document.querySelectorAll('.nav-li a[href^="#"').forEach(function (trigger) {
+    trigger.onclick = function (e) {
+      e.preventDefault();
+      var hash = this.getAttribute('href');
+      var target = document.querySelector(hash);
+      var elementPosition = target.offsetTop;
+      var offsetPosition = elementPosition - headerOffset; // reset highlight
+
+      document.querySelectorAll('.nav-li').forEach(function (li) {
+        li.className = 'nav-li';
+      });
+      var parent = this.parentNode;
+      parent.className = 'nav-li active-nav'; // scroll page
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: 'smooth'
+      });
+    };
+  });
+}
+
+function setupTimestamp(last_update) {
+  var timestamp = document.createElement('p');
+  timestamp.className = 'timestamp';
+  timestamp.innerHTML = "As of ".concat(last_update);
+  return timestamp;
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+init();
+
+/***/ })
+
+},[[79,1,2]]]);
